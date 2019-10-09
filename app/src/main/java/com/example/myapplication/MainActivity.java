7244fac01a961c81b4c159c8ae9427b9cdf14eff
Yakov9293 <yakov226805@yandex.ru>
2019-10-09 11:49:12 +0300

    Пока нет даггера, и фрагмент статичны, возможно стоит расширить их содержание на классы

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -16,6 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navSelectLitner);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navSelectLitner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId())
+            {
+                case R.id.nav_railway:
+                    selectedFragment = new RailwayFragment();
+                    break;
+                case R.id.nav_boat:
+                    selectedFragment = new BoatFragment();
+                    break;
+                case R.id.nav_car:
+                    selectedFragment = new CarFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, selectedFragment).commit();
+            return true;
+        }
+    };
 }
